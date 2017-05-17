3e9d8d061efa0cd5b0ae64e8fc0dfe6942f6beff
刘晓晨 <1459709117@qq.com>
2017-05-17 09:07:56 +0800

    项目配置

--- LiuMeiTuanIpad/LiuMeiTuanIpad/AppDelegate.m
+++ LiuMeiTuanIpad/LiuMeiTuanIpad/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "LXCBaseTabBarController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+//    self.window
+    
+    LXCBaseTabBarController *rootVc = [LXCBaseTabBarController new];
+    
+//    UINavigationController *nvc = [[UINavigationController alloc] initWithRootViewController:rootVc];
+    
+    self.window.rootViewController = rootVc;
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
